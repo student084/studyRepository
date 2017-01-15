f0aec76b24cf955d4830f6c85161f1ab418e9f10
student084 <2946465099@qq.com>
2017-01-15 16:15:31 +0800

    3 ways to connect the database ,success

--- JavaEE/JDBC_Intro/src/com/amaker/util/DBUtil.java
+++ JavaEE/JDBC_Intro/src/com/amaker/util/DBUtil.java
@@ -10,10 +10,13 @@ public class DBUtil {
 		Connection conn = util.getConnection();
 		System.out.println(conn);
 	}
+	// Connect the dateBases
 	public Connection getConnection(){
 		try {
+			//加载驱动,其中driver是类的名称
 			Class.forName("com.mysql.jdbc.Driver");
 			try {
+				//获得链接
 				return DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db", "root", "19950814Jian");
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -24,5 +27,16 @@ public class DBUtil {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	//Close a connection
+	public void closeConnection(Connection conn){
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
